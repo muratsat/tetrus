9d4748e7a0feb569224bc674c0a6b1cd0692b1cc
Murat <muratsat@users.noreply.github.com>
2024-10-12 23:31:12 +0600

    use venv in deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,15 @@ if ! git pull; then
   exit 1
 fi
 
+# setup virtual environment
+
+if ! python3 -m venv .venv; then
+  echo "Failed to create virtual environment"
+  exit 1
+fi
+
+source .venv/bin/activate
+
 if ! pip install -r requirements.txt; then
   echo "Failed to install requirements"
   exit 1
